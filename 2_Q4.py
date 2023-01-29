5ca6f39d67372559b88844c91a0edd79d21d6f16
Vivek Yadav <98008668+vivekyadav022@users.noreply.github.com>
2023-01-29 11:30:50 +0530

    Operators

---
+++ 2_Q4.py
@@ -0,0 +1,27 @@
+a=3
+b=4
+#Arithmetic operators
+print("the value of 3+4 is",3+4)
+print("the value of 3-4 is",3-4)
+print("the value of 3*4 is",3*4)
+print("the value of 3/4 is",3/4)
+#Assignment operators
+a=34
+a-=12
+a*=12
+a/=12
+print(a)
+#comparision operators
+
+#b=(14<=7)
+#b=(14>=7)
+#b=(14>7)
+#b=(14==7)
+b=(14!=7)
+print(b)
+#Logical operators
+bool1=True
+bool2=False
+print("the value of bool1 and bool2 is",bool1 and bool2)
+print("the value of bool1 or bool2",bool1 or bool2)
+print("the value of not bool2 is",(not bool2))
