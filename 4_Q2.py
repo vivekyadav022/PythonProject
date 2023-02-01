44a94c7c0397bed7862ff2fea9c2dbfecfa8cfad
Vivek Yadav <98008668+vivekyadav022@users.noreply.github.com>
2023-02-02 00:10:38 +0530

    Accept marks of six student

---
+++ 4_Q2.py
@@ -0,0 +1,9 @@
+m1=int(input("Enter marks for student number1: "))
+m2=int(input("Enter marks for student number2: "))
+m3=int(input("Enter marks for student number3: "))
+m4=int(input("Enter marks for student number4: "))
+m5=int(input("Enter marks for student number5: "))
+m6=int(input("Enter marks for student number6: "))
+myList=[m1,m2,m3,m4,m5,m6]
+myList.sort( )
+print(myList)
